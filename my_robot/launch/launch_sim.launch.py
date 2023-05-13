99e1f92379422dc3c0d42984dcb4340b0e64c4f6
leeilju <iljujjang@naver.com>
2023-05-14 05:45:44 +0900

    update gazebo_control

---
+++ my_robot/launch/launch_sim.launch.py
@@ -0,0 +1,48 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='my_robot' #<--- CHANGE ME
+
+    # ros2 launch my_robot rsp.launch.py    
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # ros2 launch gazebo_ros gazebo.launch.py
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
+
+    # ros2 run gazebo_ros spawn entity.py -topic robot_description -entity bot_name
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
+
+
+
+    # Launch them all!s
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+    ])
